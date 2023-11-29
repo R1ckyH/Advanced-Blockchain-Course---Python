71456adff4bf6520108f4b485493d9d86ddd135d
R1ckyH <836650357ho@gmail.com>
2023-11-29 18:30:37 +0800

    add random nonce

--- Blockchain/Backend/core/blockheader.py
+++ Blockchain/Backend/core/blockheader.py
@@ -3,6 +3,7 @@ Copyright (c) 2021 Codiesalert.com
 These scripts should be used for commercial purpose without Codies Alert Permission
 Any violations may lead to legal action
 """
+import random
 from Blockchain.Backend.core.database.database import BlockchainDB
 from Blockchain.Backend.util.util import (
     hash256,
@@ -14,7 +15,7 @@ from Blockchain.Backend.util.util import (
 
 
 class BlockHeader:
-    def __init__(self, version, prevBlockHash, merkleRoot, timestamp, bits, nonce = None):
+    def __init__(self, version, prevBlockHash, merkleRoot, timestamp, bits, nonce=None):
         self.version = version
         self.prevBlockHash = prevBlockHash
         self.merkleRoot = merkleRoot
@@ -33,7 +34,6 @@ class BlockHeader:
         nonce = s.read(4)
         return cls(version, prevBlockHash, merkleRoot, timestamp, bits, nonce)
 
-
     def serialize(self):
         result = int_to_little_endian(self.version, 4)
         result += self.prevBlockHash[::-1]
@@ -41,22 +41,22 @@ class BlockHeader:
         result += int_to_little_endian(self.timestamp, 4)
         result += self.bits
         result += self.nonce
-        return result 
-    
+        return result
+
     def to_hex(self):
-        self.blockHash    =  self.generateBlockHash()
-        self.nonce        =  little_endian_to_int(self.nonce)
-        self.prevBlockHash   =  self.prevBlockHash.hex()
-        self.merkleRoot  =  self.merkleRoot.hex()
-        self.bits         =  self.bits.hex()
+        self.blockHash = self.generateBlockHash()
+        self.nonce = little_endian_to_int(self.nonce)
+        self.prevBlockHash = self.prevBlockHash.hex()
+        self.merkleRoot = self.merkleRoot.hex()
+        self.bits = self.bits.hex()
 
     def to_bytes(self):
-        self.nonce        =  int_to_little_endian(self.nonce, 4)
-        self.prevBlockHash   =  bytes.fromhex(self.prevBlockHash)
-        self.merkleRoot  =  bytes.fromhex(self.merkleRoot)
-        self.blockHash    =  bytes.fromhex(self.blockHash)
-        self.bits         =  bytes.fromhex(self.bits)
-    
+        self.nonce = int_to_little_endian(self.nonce, 4)
+        self.prevBlockHash = bytes.fromhex(self.prevBlockHash)
+        self.merkleRoot = bytes.fromhex(self.merkleRoot)
+        self.blockHash = bytes.fromhex(self.blockHash)
+        self.bits = bytes.fromhex(self.bits)
+
     def mine(self, target, newBlockAvailable):
         self.blockHash = target + 1
         competitionOver = False
@@ -65,7 +65,7 @@ class BlockHeader:
             if newBlockAvailable:
                 competitionOver = True
                 return competitionOver
-                
+
             self.blockHash = little_endian_to_int(
                 hash256(
                     int_to_little_endian(self.version, 4)
@@ -76,7 +76,7 @@ class BlockHeader:
                     + int_to_little_endian(self.nonce, 4)
                 )
             )
-            self.nonce += 1
+            self.nonce = random.randint(1, 2 ** 32)
             print(f"Mining Started {self.nonce}", end="\r")
         self.blockHash = int_to_little_endian(self.blockHash, 32).hex()[::-1]
         self.nonce -= 1
@@ -88,7 +88,7 @@ class BlockHeader:
         if self.prevBlockHash.hex() == lastBlock['BlockHeader']['blockHash']:
             if self.check_pow():
                 return True
-    
+
     def check_pow(self):
         sha = hash256(self.serialize())
         proof = little_endian_to_int(sha)
@@ -98,7 +98,7 @@ class BlockHeader:
         sha = hash256(self.serialize())
         proof = little_endian_to_int(sha)
         return int_to_little_endian(proof, 32).hex()[::-1]
-    
+
     def to_dict(self):
         dt = self.__dict__
         return dt
